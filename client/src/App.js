b510267455762c29d101dcba948630f04e9e2f3a
mouveee <mouvy@web.de>
2020-02-11 15:15:22 +0100

    everything seems to be portable now

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
 			method: "POST", // *GET, POST, PUT, DELETE, etc.
 			mode: "cors", // no-cors, cors, *same-origin
 			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: "same-origin", // include, *same-origin, omit
+			credentials: "include", // include, *same-origin, omit
 			headers: {
 				"Content-Type": `application/json`,
 				Accept: "*/*",
