235ee3fcc3cc8cc39f2ae37b575dd1c6f705d64f
Marco Huwig <32535597+Mouveee@users.noreply.github.com>
2020-03-03 11:41:22 +0100

    New Bio and refactored Constructor

--- client/src/components/Biography.js
+++ client/src/components/Biography.js
@@ -1,14 +1,33 @@
 import * as React from 'react';
 
+import './Biography.css'
+
 class Biography extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { visible: this.props.visible }
+    }
+
     render(){
+        let classOfContainer = '';
+
+        if (this.state.visible) {
+          classOfContainer += 'App-mainVisibile';
+        }
+    
+        if (this.props.isMobile) {
+          classOfContainer += ' mobile';
+        }
+
         return(
-            <div>
+            <div id='App-mainPage' className={classOfContainer}>
                 Vor und während meiner Beschäftigung mit Web Development habe ich viel in der Gastronomie gearbeitet und 
                 habe mir dabei einen offenen Umgang mit Menschen angeeignet. Ich habe gern Kundenkontakt und arbeite auch 
                 an der Schnittstelle zwischen Beratung und Entwicklung. 
                 <br></br>
-                Privat beschäftige ich mich außerdem viel mit Musik und Literatur, sowohl mit Fachliteratur als auch mit Romanen.
+                Privat beschäftige ich mich viel mit Musik und lese, 
+                sowohl Fachliteratur als auch Romane. 
                 <br></br>
             </div>
     )}
