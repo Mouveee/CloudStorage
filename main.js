8b2913a5aec507d341b65e9aa623fc4837118c98
Marco Huwig <32535597+Mouveee@users.noreply.github.com>
2020-03-03 11:42:21 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-let i = 5;
-
-window.onload = function () {
-  let mainMenu = document.getElementsByClassName('sf-mainMenu');
-  this.console.log(mainMenu[0].className)
-  mainMenu[0].className += ' visible'
-  this.console.log(++i)
-}
